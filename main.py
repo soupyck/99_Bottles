e14aeec8c7b3ddd48aab9fd843f638cf39c6c3b1
Eric Campbell <soupyc@gmail.com>
2016-04-07 22:51:19 -0700

    Working Code

--- main.py
+++ main.py
@@ -12,12 +12,18 @@
 # Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
 # Put a blank line between each verse of the song.
 
-i = 99
-if i > 1:
-    print i + " bottles of beer on the wall, " + i + " bottles of beer! Take one down, pass it around..."
-    i = i - 1
-    print i + "bottles of beer on the wall!"
-elif i == 1:
-    print i + "bottles of beer on the wall, " + i + " bottles of beer! Take one down, pass it around..." + i + "bottles of beer on the wall!"
+
+for i in range (99,0, -1):
+    if i > 1:
+        print i, " bottles of beer on the wall, ", i, " bottles of beer!"
+        if i > 2:
+            suff = str(i - 1) + " bottles of beer on the wall! "
+        else:
+            suff = "1 bottle of beer on the wall!"
+    elif i == 1:
+        print "1 bottle of beer on the wall, 1 bottle of beer!"
+        suff = "no more BEER ON THE WALL!"
+    print "Take one down, pass it around,", suff
+    print "--"
 
 
